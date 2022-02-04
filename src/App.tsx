0b1277ce36653a43f5058b4f82881b7f56b0ae03
StuartFleishman <Stuart.Alex.Fleishman@gmail.com>
2022-02-04 12:45:25 -0500

    setting todos

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,20 @@ const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("");
   const [todos, setTodos] = useState<Array<Todo>>([]);
 
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
+      setTodo("");
+    }
+  };
+
   return (
     <>
     <div className="App">
       <span className="head">To-Do!</span>
-      <InputField  />
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
     </div>
     </>
   );
