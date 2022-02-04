6270f700f6983a091c0b9785a86f3d38b84459e9
StuartFleishman <Stuart.Alex.Fleishman@gmail.com>
2022-02-04 12:31:37 -0500

    added in todo, setTodo to inputField

--- src/components/InputFeild.tsx
+++ src/components/InputFeild.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import './styles.css';
 
-const InputFeild = () => {
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const InputField: React.FC<Props> = ({todo, setTodo}) => {
   return (
     <form className='input'>
       <input type="input" placeholder="Enter a task"></input>
